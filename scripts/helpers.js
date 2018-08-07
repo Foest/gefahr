e32ef484c9020b59d294e827df3e3bb6e8da2a63
Foest <foest88@gmail.com>
2018-08-07 12:40:09 -0700

    added levenshtein distance

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,11 +1,45 @@
 function checkResponse(userResponse, correctResponse) {
-    return userResponse === correctResponse;
+    return levenshtein(userResponse, correctResponse) <= 3 ? true : false;
 }
 
-function levenshtein(str1, str2){
+function minThree(a, b, c){
+    let min = a;
+    if(b < min) min = b;
+    if(c < min) min = c;
+    return min;
+}
+
+function levenshtein(s1, s2) {
+    //Remove non-alphanumeric and set lowercase
+    const str1 = s1.toLowerCase().replace(/\W/g, '');
+    const str2 = s2.toLowerCase().replace(/\W/g, '');
     const n = str1.length;
     const m = str2.length;
-    let matrix = [[]];
-    let i, j = 0;
-    console.log(i, j);
+    let matrix = new Array(n + 1);
+    for(var z = 0; z < matrix.length; z++){
+        matrix[z] = new Array(m + 1);
+    }
+    let cost;
+
+    if (n === 0) return m;
+    if (m === 0) return n;
+
+    for (var i = 0; i <= n; i++) {
+        matrix[i][0] = i;
+    }
+    for (var j = 0; j <= m; j++) {
+        matrix[0][j] = j;
+    }
+
+    for(var i = 1; i <= n; i++){
+        for(j = 1; j <= m; j++){
+            if(str1[i-1] === str2[j-1]){
+                cost = 0;
+            } else {
+                cost = 1;
+            }
+            matrix[i][j] = minThree(matrix[i-1][j] + 1, matrix[i][j-1] + 1, matrix[i-1][j-1] + cost);
+        }
+    }
+    return matrix[n][m];
 }
