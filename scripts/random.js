5ef35e8ffb63717425c84f6516f74097f3dc2482
Foest <foest88@gmail.com>
2018-08-09 10:01:58 -0700

    added difficulty mode functionality

--- scripts/random.js
+++ scripts/random.js
@@ -8,12 +8,27 @@ const nextBtn = document.querySelector('#btn-next');
 const response = document.querySelector('#response');
 const scoreDisplay = document.querySelector('#score');
 const outer = document.querySelector('.outer');
+const easyModeBox = document.querySelector('#easy-mode');
+let difficulty = "";
 let clue;
 let score = 0;
 let strikes = 0;
 
 function getData() {
-    fetch('http://127.0.0.1:3000/random_question')
+    let data = {mode: difficulty,}
+    fetch('http://127.0.0.1:3000/random_question', {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, cors, *same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, same-origin, *omit
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            // "Content-Type": "application/x-www-form-urlencoded",
+        },
+        redirect: "follow", // manual, *follow, error
+        referrer: "no-referrer", // no-referrer, *client
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+        })
         .then((res) => res.json())
         .then((data) => {
             clue = {
@@ -85,4 +100,7 @@ window.addEventListener('keyup', (e) => {
 
 submitBtn.addEventListener('click', checkAnswer);
 nextBtn.addEventListener('click', getData);
+easyModeBox.addEventListener("change", () => {
+    difficulty === "easy" ? difficulty = "" : difficulty = "easy";
+});
 getData();
